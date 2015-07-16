674bf8a3805b3583cd6eb508f1ae01ca321df0bb
Fabian Bergmark <fabian.bergmark@gmail.com>
2015-07-16 12:26:22 +0200

    New automake GHC interaction

---
+++ ffi/samples/haskell/Main.hs
@@ -0,0 +1,19 @@
+import Data.Settings.YQL
+import Data.State.YQL
+import Control.Monad.Trans.API
+import TH.APIs
+import TH.YQLs
+
+data S = S
+
+instance YQLState S where
+
+main = do
+  let input = InputSmhiPmp "58.59" "16.18"
+  (mOut, _) <- runAPIT S $ smhiPmp YQLSettings input
+  case mOut of
+   Just out -> do
+     forM (outputSmhiPmpTimeseries out) $ \ts -> do
+       let vt = outputSmhiPmpTimeseriesValidTime ts
+           t = outputSmhiPmpTimeseriesT ts
+       putStrLn (vt ++ ": " ++ (show t))
